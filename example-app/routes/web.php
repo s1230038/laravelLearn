fe5d1022f8962a724b3d79d1972db3d1e9649273
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2023-06-01 17:57:06 +0900

    [update] route parameter: List 2-9 P.44

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', [HelloController::class, 'index']);
+Route::get('hello/{id?}/{pass?}', [HelloController::class, 'index']);
